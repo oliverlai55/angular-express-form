9a15998bbc1327a5a0dc51fc5e9e2db8e6a8e3e4
Oliver Lai <oliverlai55@gmail.com>
2016-01-29 10:15:00 -0500

    added more angular

--- front-end/search.js
+++ front-end/search.js
@@ -4,4 +4,12 @@ myApp.controller('searchCntl', function($scope, $http){
 
 	$scope.url = 'http://localhost:3000/search'; //This is where Angular will look for search terms.
 
+	$scope.search = function(){
+		$http.post($scope.url, {"data" : $scope.keywords}).success(function(data, status){
+		console.log("something happened");
+		$scope.data = data;
+		$scope.status = status;
+		});
+		
+	}
 });
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 //Create a router for /search, so Angular can hit it.
-route.post('/search', function(req, res, next){
-	res.json('success');
-})
+// router.post('/search', function(req, res, next){
+// 	res.json('success');
+// });
 
+router.post('/search', function(req, res, next){
+	var searchTerms = ['peter','stuart','stephen','griffin','oliver','freddy'];
+	req.body
+	res.json(searchTerms);
+});
 module.exports = router;
